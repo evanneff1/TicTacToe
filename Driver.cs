d345e64221dde7bedfcfa8ee3121b4972ea26d34
evanneff <evanneff@EVANNEFF89FC>
2024-01-30 14:37:26 -0700

    Add project files.

---
+++ Driver.cs
@@ -0,0 +1,14 @@
+﻿namespace TicTacToe;
+internal class Driver
+{
+    private static void Main(string[] args)
+    {
+        Support bd = new Support();
+
+        bd.boardDisplay();
+        System.Console.WriteLine("Welcome to the Game!");
+
+
+
+    }
+}
---
+++ Support.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Support
+    {
+
+        public void boardDisplay() 
+        {
+            Console.WriteLine("Hello");
+        }
+    
+    }
+}
