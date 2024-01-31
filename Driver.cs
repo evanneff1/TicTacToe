0ead79e4cd76b56b63bb79c9b120cc676616555b
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-01-31 13:56:47 -0700

    This is some commented stuff

--- Driver.cs
+++ Driver.cs
@@ -1,16 +1,19 @@
 ﻿using System.Data;
 using TicTacToe;
 
+//Instatiate other class
 Support support = new Support();
 
-
+//Declare variables
 bool gameOver = false;
+string[] board = new string[9];
 
 //support.PrintBoard();
 //support.CheckWinner();
 
+//Welcome to game
 System.Console.WriteLine("Welcome to the Game!");
-string[] board = new string[9];
+
 //Print board key
 System.Console.WriteLine("1 | 2 | 3");
 System.Console.WriteLine("----------");
@@ -18,11 +21,14 @@ System.Console.WriteLine("4 | 5 | 6");
 System.Console.WriteLine("----------");
 System.Console.WriteLine("7 | 8 | 9");
 
-
+//Play game loop
 do
 {
+    //Player one turn
     Console.WriteLine("Player 1 Turn");
     string player1 = Console.ReadLine();
+
+    //Check for valid input
     while (board[int.Parse(player1) - 1] == "X" || board[int.Parse(player1) - 1] == "O")
     {
         Console.WriteLine("Spot is already taken. Please enter a new value: ");
@@ -30,31 +36,25 @@ do
 
     };
    
- 
+    //Place marker and show updated board
     board[(int.Parse(player1) - 1)] = "X";
     board.ToList().ForEach(x => Console.WriteLine(x));
 
+    //Player 2 turn
     Console.WriteLine("Player 2 Turn");
     string player2 = Console.ReadLine();
+
+    //Check valid input
     while (board[int.Parse(player2) - 1] == "X" || board[int.Parse(player2) - 1] == "O")
     {
         Console.WriteLine("Spot is already taken. Please enter a new value:");
         player2 = Console.ReadLine();
     };
 
-
+    //Place marker and show updated board
     board[(int.Parse(player2) - 1)] = "O";
     board.ToList().ForEach(x => Console.WriteLine(x));
 
     //PrintBoard()
     //CheckWinner()
-
-    Console.WriteLine();
-
-
-
-
-
-    //update board array
-    // 
 } while (!gameOver);
