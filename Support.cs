2486e517e3ee67682b4db0a58c13062c3918777d
adamjkelley01 <adamjkelley01@gmail.com>
2024-02-02 11:45:39 -0700

    added method

--- Support.cs
+++ Support.cs
@@ -13,6 +13,48 @@ namespace TicTacToe
         {
             Console.WriteLine("Hello");
         }
-    
+
+        public string CheckForWinner(char[,] board)
+        {
+            // Check rows and columns
+            for (int i = 0; i < 3; i++)
+            {
+                // Check rows
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != ' ')
+                {
+                    return board[i, 0].ToString();
+                }
+
+                // Check columns
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != ' ')
+                {
+                    return board[0, i].ToString();
+                }
+            }
+
+            // Check diagonals
+            if ((board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] ||
+                 board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]) && board[1, 1] != ' ')
+            {
+                return board[1, 1].ToString();
+            }
+
+            // Check for draw or ongoing game
+            bool isDraw = true;
+            foreach (char spot in board)
+            {
+                if (spot == ' ')
+                {
+                    isDraw = false;
+                    break;
+                }
+            }
+
+            return isDraw ? "Draw" : "None";
+        }
+
+
+
+
     }
 }
